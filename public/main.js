89a71f37073e1bb8d75384d82aa69c8bb48dc53c
TatyCat <williams.tatyanna@gmail.com>
2019-02-07 14:20:58 -0500

    Enlarged card font

--- public/main.js
+++ public/main.js
@@ -16,43 +16,6 @@ const shuffleCards = () =>{
 
 document.querySelector('.hand-down').addEventListener('click', shuffleCards)
 
-
-// when clicked, shuffle deck & display card
-
-// card-flipped
-
-
-
-
-
-
-/*
-const buildDeck = () => {
-  let ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Ace", "Jack", "Queen", "King"];
-  let suit = ["spades", "Heart", "Diamonds", "Clubs"]
-}
-
-const shuffleDeck = (ranks, suit) =>{
-  for(let i=0; i < 52; i++){
-    // let shuffleRandomizer = 
-  }
-}
-
 // for i from n - 1 down to 1 do:
 //   j = random integer (where 0 <= j <= i)
 //   swap items[i] with items[j]
-
-const main = () => {
-  //BUILD A DECK OF CARDS
-  buildDeck()
-
-  //SHUFFLES A DECK OF CARDS
-  shuffleDeck()
-
-  //DEAL A CARD
-  const button = document.querySelector
-  //listen for build card
-}
-
-document.addEventListener('DOMContentLoaded', main)
-*/
